c417ac941388122f5755521c24f8f033ceff5c2b
byang12 <byang12@crimson.ua.edu>
2021-12-05 20:45:29 -0600

    CHP4 - 7 x/y
    O(n)

--- epi_judge_python/primitive_divide.py
+++ epi_judge_python/primitive_divide.py
@@ -3,7 +3,14 @@ from test_framework import generic_test
 
 def divide(x: int, y: int) -> int:
     # TODO - you fill in here.
-    return 0
+    power = 32
+    quotient = 0
+    while x >= y:
+        while (y<<power) > x:
+            power = power - 1
+        quotient = quotient | (1<<power)
+        x = x - (y<<power)
+    return quotient
 
 
 if __name__ == '__main__':
