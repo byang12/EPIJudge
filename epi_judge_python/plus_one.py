e933289086a1b5b2357c5fb3549d18c23b44a13d
byang12 <byang12@crimson.ua.edu>
2021-12-09 15:12:46 -0600

    CHP5 - 3.plus_one - space: O(1), time: O(n)

---
+++ epi_judge_python/plus_one.py
@@ -0,0 +1,17 @@
+# p43, not found in the epi judge, tested on Leetcode,
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        i = len(digits) - 1
+        carry = 1
+        while i >= 0:
+            digits[i] = digits[i] + carry
+            if not digits[i] == 10:
+                carry = 0
+                break
+            digits[i] = 0
+            i = i - 1
+            
+        if carry == 1:
+            digits.insert(0, 1)
+        
+        return digits
