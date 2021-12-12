d65124ed9c82171a0ec5d026598b727c001e8989
byang12 <byang12@crimson.ua.edu>
2021-12-11 23:58:46 -0600

    CHP5 - 5.advance_by_offset - time: O(n)

--- epi_judge_python/advance_by_offsets.py
+++ epi_judge_python/advance_by_offsets.py
@@ -1,11 +1,16 @@
 from typing import List
-
 from test_framework import generic_test
 
 
 def can_reach_end(A: List[int]) -> bool:
     # TODO - you fill in here.
-    return True
+    furest_reach = 0
+    max_step = len(A) - 1
+    i = 0
+    while i<=furest_reach and furest_reach<=max_step:
+        furest_reach = max(furest_reach,A[i]+i)
+        i = i + 1
+    return furest_reach>=max_step
 
 
 if __name__ == '__main__':
