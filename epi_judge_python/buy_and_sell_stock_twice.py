0d4bb438214ad722bd4194ab2d0724cb7f4190ba
byang12 <byang12@crimson.ua.edu>
2021-12-15 01:02:00 -0600

    CHP5 - 7.sell and buy stock twice - time: O(n) SPace: O(n)

--- epi_judge_python/buy_and_sell_stock_twice.py
+++ epi_judge_python/buy_and_sell_stock_twice.py
@@ -5,7 +5,20 @@ from test_framework import generic_test
 
 def buy_and_sell_stock_twice(prices: List[float]) -> float:
     # TODO - you fill in here.
-    return 0.0
+    max_profit_one_sale = 0.0
+    min_price_so_far = prices[0]
+    one_sale_profit_list = [0.0]*len(prices)
+    for i in range(1,len(prices)):
+        max_profit_one_sale = max(max_profit_one_sale,prices[i]-min_price_so_far)
+        one_sale_profit_list[i] = max_profit_one_sale
+        min_price_so_far = min(prices[i],min_price_so_far)
+    
+    max_price_so_far = prices[-1]
+    max_profit = max_profit_one_sale
+    for i in reversed(range(1,len(prices)-1)):
+        max_profit = max(max_profit,one_sale_profit_list[i-1]+max_price_so_far-prices[i])
+        max_price_so_far = max(prices[i],max_price_so_far)        
+    return max_profit
 
 
 if __name__ == '__main__':
